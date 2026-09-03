38e668c86b45e8f211f4fa043af8e289f3bb49f5
brianna-zhao <jz6108@nyu.edu>
2026-09-03 09:07:31 -0400

    first commit

---
+++ week_01/js_warmup.js
@@ -0,0 +1,10 @@
+// function double(n) {
+//   return n * 2
+// }
+
+// const double = (n) => n * 2
+function shout(word) {
+  return word.toUpperCase() + '!'
+}
+
+const shout = (word) => word.toUpperCase() + '!'
