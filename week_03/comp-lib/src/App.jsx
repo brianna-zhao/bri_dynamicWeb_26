73272b68f67519ab4c9abdad851c0f3a22a81b7e
brianna-zhao <jz6108@nyu.edu>
2026-09-17 09:01:11 -0400

    week_03 class2 notes

--- week_03/comp-lib/src/App.jsx
+++ week_03/comp-lib/src/App.jsx
@@ -1,9 +1,12 @@
 import ButtonPage from './pages/ButtonPage'
 import AccordionPage from './pages/AccordionPage'
 
+// Right now App is doing the job of a page. Next week we add routes
+// so each of these gets its own url.
 const App = () => {
   return (
     <div className="container mx-auto mt-4">
+      <hr className="my-8" />
       <AccordionPage />
     </div>
   )
--- week_03/comp-lib/src/components/Accordion.jsx
+++ week_03/comp-lib/src/components/Accordion.jsx
@@ -1,6 +1,43 @@
-const Accordion = () => {
+import {useState} from 'react'
+import {GoChevronDown, GoChevronUp} from 'react-icons/go'
+
+
+
+const Accordion = (props) => {
+    const{items} = props
+
+    const [expandedIndex, setExpandedIndex] = useState(-1)
+
+    const handleClick = (nextIndex) =>{
+        setExpandedIndex ((currentExpandedIndex)=>{
+            if(currentExpandedIndex === nextIndex){
+                return -1
+            }
+            return nextIndex
+        })
+    }
+    
+const renderedItems = items.map((item, index)=>{
+    const isExpanded = expandedIndex === index
+
+    const icon = isExpanded ? <GoChevronDown/> : <GoChevronUp/>
+
+    return(
+        <div key={item.id}>
+            <div 
+            onClick= {() => handleClick(index)} 
+            className="flex justify-between items-center p-3 bg-gray-100 border-b">
+                {item.label}
+                {icon}
+            </div>
+            {isExpanded && <div className="border-b p-5">{item.content}</div>}        
+        </div>
+        )
+    })
+
+
   return (
-    <div>Accordion</div>
+    <div>{renderedItems}</div>
   )
 }
 
